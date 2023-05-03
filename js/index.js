1836cce9022f356794c277d1dc6e56e376d25190
dzymenko georgy <erterj05@gmail.com>
2023-05-03 18:36:02 +0300

    solution

--- js/index.js
+++ js/index.js
@@ -1,5 +1,49 @@
 // import {innerHTML} from "./writeToTeacher";
 
+import Babiuk from "../assets/teachers/babiuk.jpeg"
+import Denysiuk from "../assets/teachers/denysiuk.jpeg"
+import Kyrylaschuk from "../assets/teachers/kyrylaschuk.jpeg"
+import Radomska from "../assets/teachers/radomska.jpeg"
+import Voitko from "../assets/teachers/voitko.jpeg"
+
+const teachers = [
+    {
+      img: Kyrylaschuk,
+      name: "Кирилащук Світлана Аналоліївна",
+      position: "декан",
+    },
+    {
+      img: Babiuk,
+      name: "Бабюк Наталя Петрівна",
+      position: "доцент",
+    },
+    {
+      img: Denysiuk,
+      name: "Денисюк Алла Василівна",
+      position: "асистент"
+    },
+    {
+      img: Voitko,
+      name: "Войтко Вікторія Володимирівна",
+      position: "доцент"
+    },
+    {
+      img: Radomska,
+      name: "Радомська Людмила Анатоліївна",
+      position: "доцент"
+    }
+]
+
+const onClick = (event) => {
+    console.log(event.clientX)
+}
+
+const onSearch = (event) => {
+    // ul.send-message__serched-list
+    console.log("a");
+}
+
 document.addEventListener("DOMContentLoaded", function() {
-    
+    document.querySelector("input.input-container__input").addEventListener("input", onSearch)
+    document.querySelector("#send-button").addEventListener("click", onClick);
 })
